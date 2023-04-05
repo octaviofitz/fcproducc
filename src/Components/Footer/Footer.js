2f3aba29f84db12285c306a8643fe1efa41459be
octaviofitz <octaviofitz@gmail.com>
2023-04-04 21:51:13 -0300

    cards

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import '../Footer/footer.css'
+
 function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className='footer'>
+            
+            <div>
+                <p>Contacto</p>
+                <p>Whatsapp: 11 5668-8314</p>
+                <p>Correo: info@fcproducciones.com.ar</p>
+            </div>
+
+            <div className='subcontainer'>
+           
+            <div>
+                <p>Seguinos</p>
+                <p>Seguinos</p>
+                </div>
+
+                <div>
+                    <p>Menu</p>
+                    <p>Menu</p>
+                    <p>Menu</p>
+                </div>
+
+                </div>
         </div>
     );
 }
